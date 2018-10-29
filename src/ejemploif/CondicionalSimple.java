5e83a5fe54b0331e72af4c28e4d4b495916fd489
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-10-29 11:36:54 +0100

    1

---
+++ src/ejemploif/CondicionalSimple.java
@@ -0,0 +1,24 @@
+package ejemploif;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author drodriguezguardiola
+ */
+public class CondicionalSimple {
+    
+    public void maiorEdade(){
+        
+    Scanner res = new Scanner(System.in);
+    
+    System.out.println("teclea edade :");
+    
+    int edade = res.nextInt();
+    
+    if(edade<18){
+    
+        System.out.println(" e menor de edade");
+}  
+}
+}
